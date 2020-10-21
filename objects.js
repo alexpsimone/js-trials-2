379329ea0907bf76ce4359b82a1948c3bc678660
alexpsimone <alex.p.simone@gmail.com>
2020-10-21 00:57:52 +0000

    completed getMelonsAtPrice func

--- objects.js
+++ objects.js
@@ -21,5 +21,19 @@ function countWords(phrase) {
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
+
+  const melonPrices = {
+    2.50 : ['Cantaloupe', 'Honeydew'],
+    2.95 : ['Watermelon'],
+    3.25 : ['Musk', 'Crenshaw'],
+    14.25 : ['Christmas']
+    };
+  
+    if (melonPrices.hasOwnProperty(price)) {
+      return melonPrices[price];
+    } else {
+      console.log('None found.')
+    }
+    
   // Replace this with your code
 }
