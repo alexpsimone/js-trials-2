e52efeb68e194c6c944b2239fce6bbd4a3230292
alexpsimone <alex.p.simone@gmail.com>
2020-10-21 00:21:05 +0000

    completed calculateTotal func

--- functions.js
+++ functions.js
@@ -16,5 +16,22 @@ function getFullName(firstName, lastName){
 
 
 // 3. calculateTotal
+function calculateTotal(basePrice, state, tax=0.05) {
+    
+    const subTotal = basePrice * (1 + tax);
+    let fee = 0;
 
+    if (state ==='CA') {
+        fee = 0.03 * subTotal;
+    } else if (state === 'PA') {
+        fee = 2;
+    } else if (state === 'MA') {
+        if (basePrice <= 100) {
+            fee = 1;
+        } else {
+            fee = 3;
+        }
+    }
+    return subTotal + fee;
+}
 // Define your function here
