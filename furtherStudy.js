ecfc071bb6d18d749babaade262900925830b2ed
alexpsimone <alex.p.simone@gmail.com>
2020-10-21 01:00:27 +0000

    completed getMelonsAtPrice func

--- furtherStudy.js
+++ furtherStudy.js
@@ -2,7 +2,18 @@
 
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+
+  const words1 = new Set(words1);
+  const words2 = new Set(wrods2);
+  let result = new Set();
+
+  for (words of words1) {
+    for (word of words2) {
+      result.add(word);
+    }
+  }
+
+  return result;
 }
 
 
